43eb6b0d672eedf142706ef56196eae69f754983
viparosh <protectedstaticfunction@gmail.com>
2023-09-16 07:33:29 +0800

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,36 @@
+import fileIcon from './file.png'
+import swatch from './swatch.png'
+import download from './download.png'
+
+import logoShirt from './logo-tshirt.png'
+import stylishShirt from './stylish-tshirt.png'
+import logoControls from './logo-controls.png'
+import textIcon from './text-icon.png'
+import textureLogoPicker from './textureLogoPicker.jpeg'
+import texture1 from './texture1.jpeg'
+import texture2 from './texture2.jpeg'
+import texture3 from './texture3.jpeg'
+import texture4 from './texture4.jpeg'
+import texture5 from './texture5.jpeg'
+import logo1 from './logo1.png'
+import logo2 from './logo2.png'
+import logo3 from './logo3.jpg'
+
+export {
+  logo3,
+  fileIcon,
+  swatch,
+  download,
+  logoShirt,
+  stylishShirt,
+  logoControls,
+  textIcon,
+  textureLogoPicker,
+  texture1,
+  texture2,
+  texture3,
+  texture4,
+  texture5,
+  logo1,
+  logo2,
+}
---
+++ src/canvas/Shirt.jsx
@@ -0,0 +1,102 @@
+import React from 'react'
+import * as THREE from 'three';
+import { easing } from 'maath';
+import { useSnapshot } from 'valtio';
+import { useFrame, useThree } from '@react-three/fiber';
+import { Decal, useGLTF, useTexture, OrbitControls } from '@react-three/drei';
+
+import state from '../store';
+
+const Shirt = () => {
+  const snap = useSnapshot(state);
+  const { nodes, materials } = useGLTF('/shirt.glb');
+
+  const logoTexture = useTexture(snap.frontLogoDecal);
+  const fullTexture = useTexture(snap.fullDecal);
+  const backLogoTexture = useTexture(snap.backLogoDecal);
+
+  useFrame((state, delta) => easing.dampC(materials.lambert1.color, snap.color, 0.25, delta));
+
+  const stateString = JSON.stringify(snap);
+
+  const createTextTexture = (text, font, size, color) => {
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    ctx.font = `${size}px ${font}`;
+    const textWidth = ctx.measureText(text).width;
+    canvas.width = textWidth;
+    canvas.height = size;
+    ctx.fillStyle = color;
+    ctx.font = `${size}px ${font}`;
+    ctx.fillText(text, 0, size);
+    return new THREE.CanvasTexture(canvas);
+  }
+
+  return (
+    <>
+      <OrbitControls />
+      <group key={stateString}>
+        <mesh
+          geometry={nodes.T_Shirt_male.geometry}
+          material={materials.lambert1}
+          // material-roughness={0}
+          material-metalness={0.1}
+          dispose={null}
+        >
+          {snap.isFullTexture && (
+            <Decal
+              position={[0, 0, 0]}
+              rotation={[0, 0, 0]}
+              scale={1}
+              map={fullTexture}
+              depthTest={false}
+              depthWrite={true}
+            />
+          )}
+
+          {snap.isFrontLogoTexture && (
+              <Decal
+                position={snap.frontLogoPosition}
+                rotation={[0, 0, 0]}
+                scale={snap.frontLogoScale}
+                map={logoTexture}
+                map-anisotropy={16}
+                depthTest={false}
+                depthWrite={true}
+              />
+          )}
+          {snap.isFrontText && (
+          <Decal
+            position={snap.frontTextPosition}
+            rotation={snap.frontTextRotation}
+            scale={snap.frontTextScale}
+            map={createTextTexture(snap.frontText, snap.frontTextFont, snap.frontTextSize, snap.frontTextColor)}
+          />
+          )}
+
+          {snap.isBackLogoTexture && (
+            <Decal
+              position={snap.backLogoPosition}
+              rotation={snap.backLogoRotation}
+              scale={snap.backLogoScale}
+              map={backLogoTexture}
+              map-anisotropy={16}
+              depthTest={false}
+              depthWrite={true}
+            />
+          )}
+          {snap.isBackText && (
+            <Decal
+              position={snap.backTextPosition}
+              rotation={snap.backTextRotation}
+              scale={snap.backTextScale}
+              map={createTextTexture(snap.backText, snap.backTextFont, snap.backTextSize, snap.backTextColor)}
+            />
+          )}
+        </mesh>
+      </group>
+    </>
+  );
+}
+
+export default Shirt
